e1f7c17c2c4bf6eb42e20728e0b5c37d3a0d1fe7
viranarullita <viranarullita3@gmail.com>
2025-06-04 20:19:30 +0700

    Pesan commit yang menjelaskan perubahan

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import Buah from "./components/buah";
+import Buah from "./components/Buah";
 import Footer from "./components/Footer";
-import Header from "./components/header";
-
+import Header from "./components/Header";
 
 function App() {
+  const dataBuah = [
+    { nama: "Alpukat", warna: "Hijau", gambar: "alpukat.png", harga: "15000" },
+    { nama: "Anggur", warna: "Ungu", gambar: "anggur.png", harga: "15000" },
+    { nama: "Jambu", warna: "Biru", gambar: "jambu.png", harga: "15000" },
+    { nama: "Jeruk", warna: "Oranye", gambar: "jeruk.png", harga: "15000" },
+    { nama: "Kiwi", warna: "Coklat", gambar: "kiwi.png", harga: "15000" },
+    { nama: "Lemon", warna: "Kuning", gambar: "lemon.png", harga: "15000" },
+  ];
+
   return (
     <>
-      <Header/>
-      <Buah/>
-      <Footer/>
+      <Header />
+      <Buah buahList={dataBuah} />
+      <Footer />
     </>
   );
 }
--- src/components/Buah.jsx
+++ src/components/Buah.jsx
@@ -1,58 +1,15 @@
-const Buah = () => {
-  const dataBuah = [
-    {
-      nama: "Alpukat",
-      warna: "Hijau",
-      gambar: "alpukat.png",
-      harga: "15000",
-    },
-    {
-      nama: "Anggur",
-      warna: "Ungu",
-      gambar: "anggur.png",
-      harga: "15000",
-    },
-    {
-      nama: "Jambu",
-      warna: "Biru",
-      gambar: "jambu.png",
-      harga: "15000",
-    },
-    {
-      nama: "Jeruk",
-      warna: "Oranye",
-      gambar: "jeruk.png",
-      harga: "15000",
-    },
-    {
-      nama: "Kiwi",
-      warna: "Coklat",
-      gambar: "kiwi.png",
-      harga: "15000",
-    },
-    {
-      nama: "Lemon",
-      warna: "Kuning",
-      gambar: "lemon.png",
-      harga: "15000",
-    },
-  ];
-
+const Buah = ({ buahList }) => {
   return (
     <main>
       <div className="buah-container">
         <h2>Daftar Buah</h2>
         <div className="buah-grid">
-          {dataBuah.map((buahh, index) => (
+          {buahList.map((buah, index) => (
             <div className="buah-card" key={index}>
-              <img
-                src={buahh.gambar}
-                alt={buahh.nama}
-                className="buah-gambar"
-              />
-              <h3>{buahh.nama}</h3>
-              <p>Warna: {buahh.warna}</p>
-              <p>Harga: Rp {buahh.harga}</p>
+              <img src={buah.gambar} alt={buah.nama} className="buah-gambar" />
+              <h3>{buah.nama}</h3>
+              <p>Warna: {buah.warna}</p>
+              <p>Harga: Rp {buah.harga}</p>
             </div>
           ))}
         </div>
